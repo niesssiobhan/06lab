f24d6fb7d999ed48e6fe88d409e530acc06a2a02
tyler <tyler.r.hood@gmail.com>
2018-11-20 10:45:21 -0800

    added basic server.js

---
+++ server.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const express = require('express');
+const cors = require('cors');
+
+require('dotenv').config();
+
+const PORT = process.env.PORT;
+const app = express();
+app.use(cors());
