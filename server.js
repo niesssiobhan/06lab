15c8f877b69b5bc6fab3203d3aaad6b18c65abf8
tyler <tyler.r.hood@gmail.com>
2018-11-20 11:24:09 -0800

    added data file and the basick proof of life on server.js

--- server.js
+++ server.js
@@ -8,3 +8,25 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
+app.get('/location', (request, response) => {
+  console.log(request.query.data, 'is the query that came from the search field in the browser.');
+  const locationData = searchToLatLong(request.query.data);
+  console.log(locationData);
+  response.send(locationData);
+})
+
+function searchToLatLong(query) {
+  const geoData = require('./data/geo.json');
+  const location = new Location(geoData.results[0]);
+  location.search_query = query;
+  return location;
+}
+
+
+function Location(data) {
+  this.formatted_query = data.formatted_address;
+  this.latitude = data.geometry.location.lat;
+  this.longitdue = data.geometry.location.lng;
+}
+
+app.listen(PORT, () => console.log(`App is up on ${PORT}`));
