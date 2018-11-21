ccd1aa51a07cadeb0d3a2e4a54658cb7d710c612
niesssiobhan <niesssiobhan@live.com>
2018-11-20 16:36:28 -0800

    working on map

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const express = require('express');
+const superagent = require('superagent');
 const cors = require('cors');
 
 require('dotenv').config();
@@ -8,25 +9,36 @@ require('dotenv').config();
 const PORT = process.env.PORT;
 const app = express();
 app.use(cors());
+
 app.get('/location', (request, response) => {
-  console.log(request.query.data, 'is the query that came from the search field in the browser.');
-  const locationData = searchToLatLong(request.query.data);
-  console.log(locationData);
-  response.send(locationData);
-})
+  searchToLatLong(request.query.data)
+    .then((location) => response.send(location))
+    .catch((error) => handleError(error, response));
+});
 
 function searchToLatLong(query) {
-  const geoData = require('./data/geo.json');
-  const location = new Location(geoData.results[0]);
-  location.search_query = query;
-  return location;
+  const url = `https://maps.googleapis.com/maps/api/geocode/json?address=${query}&key=${process.env.GEOCODE_API__KEY}`
+
+  return superagent.get(url)  //this is our proxy
+  .then((res) => {
+    // console.log(res.body.results[0]);
+    return new Location(query, res.body.results[0]);
+  })
+  .catch((error, res) => handleError(error, res));
+}
+
+function handleError(error, res) {
+  console.error(error);
+  if (res) res.status(500).send('sorry something broke');
 }
 
 
-function Location(data) {
+function Location(query, data) {
+  this.search_query = query;
   this.formatted_query = data.formatted_address;
   this.latitude = data.geometry.location.lat;
   this.longitude = data.geometry.location.lng;
+  console.log(this);
 }
 
 app.listen(PORT, () => console.log(`App is up on ${PORT}`));
